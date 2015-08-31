a3d0755429ac0b08aaf3849b7fdee0379c092a73
Kevin Seghetti <kts@tenetti.org>
2015-08-31 01:45:26 -0700

    added db file to list of files for rsync to ignore

--- bdsm_checklist/deploy.sh
+++ bdsm_checklist/deploy.sh
@@ -2,7 +2,7 @@
 
 # scp -r * tenettiquiz2@tenetti.org:~/quiz.tenetti.org/quiz
 
-rsync --exclude \.svn --exclude 'bdsm_checklist.vp*' --exclude passenger_swgi.py  -axvz . -e ssh tenettiquiz2@tenetti.org:~/quiz.tenetti.org/quiz
+rsync --exclude '*.sq3'  --exclude 'bdsm_checklist.vp*' --exclude passenger_swgi.py  -axvz . -e ssh tenettiquiz2@tenetti.org:~/quiz.tenetti.org/quiz
 scp passenger_swgi.py  tenettiquiz2@quiz.tenetti.org:~/quiz.tenetti.org
 ssh  tenettiquiz2@quiz.tenetti.org "source venv/bin/activate.csh ; cd quiz.tenetti.org/quiz; ./manage.py collectstatic --noinput"
 ssh  tenettiquiz2@quiz.tenetti.org "touch quiz.tenetti.org/tmp/restart.txt"
