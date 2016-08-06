c4f6bae0bc033a6f309c3a4f55642a70d06786d6
KevinSeghetti <kts@tenetti.org>
2016-08-06 12:06:20 -0700

    added django rest framework

---
+++ bdsm_checklist/checklist/serializers.py
@@ -0,0 +1,16 @@
+from checklist.models import Question, Answer
+from rest_framework import serializers
+
+
+class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Question
+        fields = ('question_text', 'question_detail',)
+
+class AnswerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Answer
+        fields = ('answer_test', 'answer_detail',)
+
+
+
