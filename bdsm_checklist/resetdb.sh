2a36fefd1fe571704d04f5469e8fa3f1d81738e3
KevinSeghetti <kts@tenetti.org>
2016-08-13 12:26:52 -0700

    fixed resetdb.sh script

--- bdsm_checklist/resetdb.sh
+++ bdsm_checklist/resetdb.sh
@@ -4,9 +4,8 @@
 dropdb bdsm_checklist
 createuser bdsm_checklist
 
-su -c 'su - postgres' <<EOT
 psql -c 'create database bdsm_checklist  ;'
-psql
+psql <<EOT
 CREATE USER bdsm_checklist WITH PASSWORD 'oj9387gy2hb0s';
 GRANT ALL PRIVILEGES ON DATABASE bdsm_checklist to bdsm_checklist;
 \q
