bbde32acb78fa4c8794eaa067f758b853e00419f
Kevin Seghetti <kts@tenetti.org>
2016-09-15 22:42:13 -0700

    moved choice data from server side into an applicationData module

---
+++ bdsm_checklist/assets/js/applicationData.jsx
@@ -0,0 +1,9 @@
+
+// bridge server side data into a module
+// this contains data from the server side, that doesn't change
+// per app, but is served from the server side so we don't repeat ourselves
+
+export var choices = window.globs['choices']
+export var choices_context = window.globs['choices_context']
+export var csrfToken = window.globs['csrfToken']
+
