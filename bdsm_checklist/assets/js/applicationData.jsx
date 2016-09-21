7376d526fd777be9a8be78d0040cc12136512d89
Kevin Seghetti <kts@tenetti.org>
2016-09-21 15:47:06 -0700

    updated logging system to take level settings per log

--- bdsm_checklist/assets/js/applicationData.jsx
+++ bdsm_checklist/assets/js/applicationData.jsx
@@ -6,4 +6,6 @@
 export var choices = window.globs['choices']
 export var choices_context = window.globs['choices_context']
 export var csrfToken = window.globs['csrfToken']
+export var defaultLoggingLevel = window.globs['defaultLoggingLevel']
+export var loggingLevels = window.globs['loggingLevels']
 
