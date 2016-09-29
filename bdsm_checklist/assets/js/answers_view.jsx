15ebbff6337e7a93597ab7f926f562b4cb11e5cf
Kevin Seghetti <kts@tenetti.org>
2016-09-29 08:55:21 -0700

    checkpoint: moved reducers to seperate files

--- bdsm_checklist/assets/js/answers_view.jsx
+++ bdsm_checklist/assets/js/answers_view.jsx
@@ -12,124 +12,13 @@ var AnswerPage = require('./AnswersView').AnswerPage
 var {choices, choices_context} = require('./applicationData')
 import { ACTION_LOAD,ACTION_SET_BOOLEAN_FILTER,ACTION_SET_RATING_FILTER,ACTION_SET_SEARCH_STRING } from './actionTypes'
 import { loadAction, toggleBooleanFilterAction,  toggleRatingFilterAction } from './actionTypes'
+import { topReducer } from './topReducer'
 import { mapObject } from './utilities'
-var AnswerPage = require('./AnswersView').AnswerPage
 
 var url=window.globs['questionsUrl']
 
 //===============================================================================
 
-const initialState = {
-  questions: [],
-  filters:
-  {
-      'to_me': {
-          booleans: {},
-          rating: {}
-      },
-      'to_others': {
-          booleans: {},
-          rating: {}
-      }
-  }
-}
-
-
-//-------------------------------------------------------------------------------
-
-function contextBooleanFilterReducer(state,action) {
-    log.trace("contextlBooleanFilterReducer: filter state = ",JSON.stringify(state,null,2))
-    let newValue = true
-    if(action.id in state) {
-        newValue = !state[action.id]
-    }
-    let result = Object.assign({}, state, { [action.id] : newValue })
-    log.debug("contextBooleanFilterReducer:ACTION_SET_BOOLEAN_FILTER: result ",JSON.stringify(result,null,2))
-    return result
-
-}
-
-function contextRatingFilterReducer(state,action) {
-    log.trace("contextlBooleanFilterReducer: filter state = ",JSON.stringify(state,null,2))
-    let newValue = true
-    if(action.rating in state) {
-        newValue = !state[action.rating]
-    }
-    let result = Object.assign({}, state, { [action.rating] : newValue })
-    log.debug("contextBooleanFilterReducer:ACTION_SET_BOOLEAN_FILTER: result ",JSON.stringify(result,null,2))
-    return result
-
-}
-
-function contextFilterReducer(state,action) {
-    log.trace("contextFilterReducer: filter state = ",JSON.stringify(state,null,2))
-    switch (action.type) {
-        case ACTION_SET_BOOLEAN_FILTER:
-            {
-                let result = mapObject(state, (contents,key) => {
-                    if (key == 'booleans') {
-                        return contextBooleanFilterReducer(state[key],action)
-                    }
-                    return contents
-                })
-                log.debug("contextFilterReducer:ACTION_SET_BOOLEAN_FILTER: result ",JSON.stringify(result,null,2))
-                return result
-            }
-        case ACTION_SET_RATING_FILTER:
-            {
-                let result = mapObject(state, (contents,key) => {
-                    if (key == 'rating') {
-                        return contextRatingFilterReducer(state[key],action)
-                    }
-                    return contents
-                })
-                log.debug("contextFilterReducer:ACTION_SET_BOOLEAN_FILTER: result ",JSON.stringify(result,null,2))
-                return result
-            }
-        default:
-          return state
-    }
-}
-
-function filterReducer(state,action) {
-    log.trace("filterReducer: filter state = ",JSON.stringify(state,null,2))
-    switch (action.type) {
-        case ACTION_SET_BOOLEAN_FILTER:
-        case ACTION_SET_RATING_FILTER:
-            let result = mapObject(state, (contents,key) => {
-                if (key == action.context) {
-                    return contextFilterReducer(state[key],action)
-                }
-                return contents
-            })
-            log.debug("filterReducer:ACTION_SET_BOOLEAN_FILTER: result ",JSON.stringify(result,null,2))
-            return result
-
-        default:
-          return state
-    }
-}
-
-function topReducer(state = initialState, action) {
-
-    log.trace("topReducer: ",JSON.stringify(action,null,2))
-    switch (action.type) {
-    case ACTION_LOAD:
-
-        return Object.assign({}, state, {
-            questions: action.data.results
-        })
-    case ACTION_SET_BOOLEAN_FILTER:
-    case ACTION_SET_RATING_FILTER:
-        let result = Object.assign({}, state, {
-          filters: filterReducer(state.filters, action)
-        })
-        log.debug("topReducer:ACTION_SET_BOOLEAN_FILTER: result ",JSON.stringify(result,null,2))
-        return result
-    default:
-      return state
-    }
-}
 
 
 export var AnswerBox = React.createClass({
---
+++ bdsm_checklist/assets/js/topReducer.js
@@ -0,0 +1,54 @@
+//===============================================================================
+// top level state reducer
+
+var chai = require('chai')
+var log = require('./loggingConfig').CreateLogger("reducers")
+
+var {choices, choices_context} = require('./applicationData')
+import { ACTION_LOAD,ACTION_SET_BOOLEAN_FILTER,ACTION_SET_RATING_FILTER,ACTION_SET_SEARCH_STRING } from './actionTypes'
+import { loadAction, toggleBooleanFilterAction,  toggleRatingFilterAction } from './actionTypes'
+import { mapObject } from './utilities'
+import { filterReducer } from './filterReducers'
+
+//===============================================================================
+
+const initialState = {
+  questions: [],
+  filters:
+  {
+      'to_me': {
+          booleans: {},
+          rating: {}
+      },
+      'to_others': {
+          booleans: {},
+          rating: {}
+      }
+  }
+}
+
+//-------------------------------------------------------------------------------
+
+export function topReducer(state = initialState, action) {
+
+    log.trace("topReducer: ",JSON.stringify(action,null,2))
+    switch (action.type) {
+    case ACTION_LOAD:
+
+        return Object.assign({}, state, {
+            questions: action.data.results
+        })
+    case ACTION_SET_BOOLEAN_FILTER:
+    case ACTION_SET_RATING_FILTER:
+        let result = Object.assign({}, state, {
+          filters: filterReducer(state.filters, action)
+        })
+        log.debug("topReducer:ACTION_SET_BOOLEAN_FILTER: result ",JSON.stringify(result,null,2))
+        return result
+    default:
+      return state
+    }
+}
+
+//===============================================================================
+
