18f0060e1c2767206eafd2dd08cad002b9261a08
Kevin Seghetti <kts@tenetti.org>
2016-09-30 10:47:51 -0700

    added webpack to post deploy

--- bdsm_checklist/post_deploy.sh
+++ bdsm_checklist/post_deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+node_modules/.bin/webpack -p
+
 ./manage.py migrate -v 0
 ./manage.py collectstatic --noinput -i jsx -i '*.scss' -v 0
 ./manage.py collectstatic --clear --noinput -i jsx -i '*.scss' -v 0
