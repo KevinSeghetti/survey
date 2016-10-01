38186eba1c9aaab905e8670fb29fd7545645c774
Kevin Seghetti <kts@tenetti.org>
2016-10-01 16:10:37 -0700

    added pipsetup, which installs all pip modules listed in autoinstall.manifest

---
+++ survey/pipsetup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# this script installs all python libraries listed in the manifest
+
+# kts todo: pip install doesn't work on the mac, those packages were installed with another package manager
+
+import os
+import sys
+import pprint
+import pip
+
+def install(package):
+    print("installing module named",package)
+    pip.main(['install', package])
+
+# instal pyaml before trying to use it
+install('pyaml')
+
+import yaml
+
+with open('autoinstall.manifest', 'r') as f:
+    doc = yaml.load(f)
+
+for module in doc.get('pip',[]):
+    install(module)
