20e33a9b3fa97dfc4a501730b2d2e0046297f954
Will Norris <will@biohack.net>
2016-10-01 23:23:46 -0700

    separate to script to avoid dealing with make issues atm

---
+++ debian/make-deb.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export RAILS_ENV=production
+cd ../survey && ./pipsetup.py	# need local copies
+pwd
+cd ../survey/survey && npm install
+pwd
+export GEMS_HOME=`pwd`/pip/
+cd ../survey && ( ./node_modules/.bin/webpack -p; ./manage.py migrate -v 0; ./manage.py collectstatic --noinput -i jsx -i '*.scss' -v 0; ./manage.py collectstatic --clear --noinput -i jsx -i '*.scss' -v 0; ./manage.py compilestatic )
+export VERSION=1.0-1
+export APP_DIR=app_${VERSION}
+sudo rm -rf package ; mkdir package && cd package
+pwd
+mkdir -p ${APP_DIR}/DEBIAN
+cp ../control ${APP_DIR}/DEBIAN/control
+export DEB_APP_DIR=${APP_DIR}/opt/app/
+mkdir -p ${DEB_APP_DIR}
+cp -R ../.bundle/ ${DEB_APP_DIR}
+rsync -avz --exclude 'app/assets/' ../app ${DEB_APP_DIR}
+cp -R ../bin/ ${DEB_APP_DIR}
+cp -R ../config/ ${DEB_APP_DIR}
+cp -R ../config.ru ${DEB_APP_DIR}
+cp -R ../db/ ${DEB_APP_DIR}
+cp -R ../Gemfile* ${DEB_APP_DIR}
+cp -R ../gems/ ${DEB_APP_DIR}
+cp -R ../lib/ ${DEB_APP_DIR}
+cp -R ../log/ ${DEB_APP_DIR}
+cp -R ../public/ ${DEB_APP_DIR}
+mkdir ${DEB_APP_DIR}/public/uploads
+cp -R ../Rakefile ${DEB_APP_DIR}
+-sudo adduser --disabled-password deployer
+sudo chown -R deployer:staff ${APP_DIR}
+dpkg-deb -v --build app_${VERSION}
