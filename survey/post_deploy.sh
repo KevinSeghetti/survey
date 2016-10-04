f5f46ab49170a66584505c51cfad2fc68ee8a092
Kevin Seghetti <kts@tenetti.org>
2016-10-03 18:34:26 -0700

    added permissions updates to post_deploy.sh

--- survey/post_deploy.sh
+++ survey/post_deploy.sh
@@ -8,6 +8,11 @@ node_modules/.bin/webpack -p
 ./manage.py collectstatic --clear --noinput -i jsx -i '*.scss' -v 0
 ./manage.py compilestatic
 
+# sqlite needs to be able to write to parent directory of sq3 file
+sudo chgrp www-data survey
+sudo chgrp www-data survey/bdsm_checklist.sq3
+sudo chgrp www-data survey/sports_checklist.sq3
+
 sudo service apache2 restart
 
 
