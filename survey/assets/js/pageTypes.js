e497166dafa79dabf6e21ffeff813d3f4edf258b
Kevin Seghetti <kts@tenetti.org>
2016-10-11 00:55:53 -0700

    fixed one page app initial page setting

--- survey/assets/js/pageTypes.js
+++ survey/assets/js/pageTypes.js
@@ -1,5 +1,5 @@
 
-export const PAGE_MAIN            = 'Home'
+export const PAGE_HOME            = 'Home'
 export const PAGE_ANSWER_EDITOR   = "AnswerEditor"
 export const PAGE_ANSWER_VIEWER   = "AnswerViewer"
 export const PAGE_QUESTION_VIEWER = "QuestionViewer"
