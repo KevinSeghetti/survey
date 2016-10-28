4f2e52f71c66e2764367f5507cd7f3a72acf90ec
Kevin Seghetti <kts@tenetti.org>
2016-10-27 20:22:04 -0700

    beginning of tests

---
+++ survey/__tests__/filterReducers.test.js
@@ -0,0 +1,55 @@
+console.log("!!!!!!!!!!!!!!!")
+
+//import { contextBooleanFilterReducer } from '../assets/js/filterReducers.js'
+//import { ACTION_LOAD,ACTION_SET_BOOLEAN_FILTER,ACTION_SET_RATING_FILTER,ACTION_SET_SEARCH_STRING } from '../assets/js/actionTypes'
+
+global.fred = "Hello"
+global.window = {}
+global.window.globs = []
+
+
+console.log("!!!!!!!!!!!!!!!")
+//
+//global.window.globs['choices'] = {"rating": [{"name": "na", "description": "N/A"}, {"name": "love", "description": "Love"}, {"name": "like", "description": "Like"}, {"name": "dont_mind", "description": "Don't Mind"}, {"name": "dislike", "description": "Dislike"}, {"name": "hate", "description": "Hate"}], "booleans": [{"name": "essential", "description": "Essential"}, {"name": "curious", "description": "Curious"}, {"name": "soft_limit", "description": "Soft Limit"}, {"name": "hard_limit", "description": "Hard Limit"}, {"name": "have_done", "description": "Have Done"}]}
+//global.window.globs['choices_context'] = [{"name": "to_me", "description": "To/For Self"}, {"name": "to_others", "description": "To/For Others"}]
+//global.window.globs['csrfToken'] = "csrf_token"
+//global.window.globs['defaultLoggingLevel'] = "OFF"
+//var loggingLevel='ERROR'    // 'ALL'
+//window.globs['loggingLevels'] =
+//{
+//    'AnswerView' : loggingLevel,
+//    //'viewerComponents' : loggingLevel,
+//    'answers_view' : loggingLevel,
+//    'actions' : loggingLevel,
+//    'reducers' : loggingLevel,
+//}
+//window.globs['questionsUrl'] = "questionsUrl"
+//
+test('contetRatingFilterReducer', () => {
+//
+//  let input =
+//  {
+//      "essential": true,
+//      "curious": true,
+//      "soft_limit": true,
+//      "hard_limit": true,
+//      "have_done": true
+//    }
+//  let action =
+//  {
+//      type: ACTION_SET_BOOLEAN_FILTER,
+//      id: 'have_done'
+//
+//  }
+//  let output = contextBooleanFilterReducer(input,action)
+//  expect(ouput).toEqual(
+//        {
+//          "essential": true,
+//          "curious": true,
+//          "soft_limit": true,
+//          "hard_limit": true,
+//          "have_done": false
+//        }
+//      )
+});
+
