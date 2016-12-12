63b928e9058ac36a9de94d93a14bd4b4e4ddba5e
Kevin Seghetti <kts@tenetti.org>
2016-12-11 16:00:22 -0800

    first working filter reducer tests

--- survey/__tests__/filterReducers.test.js
+++ survey/__tests__/filterReducers.test.js
@@ -1,14 +1,30 @@
 
-import  contextBooleanFilterReducer  from 'filterReducers'
+import reducer from 'topReducerAnswerEditor'
+import {
+    contextBooleanFilterReducer,
+    contextRatingFilterReducer,
+    filterReducer
+    } from 'filterReducers'
 import * as types from 'constants/actionTypes';
+import {
+    ACTION_TOGGLE_BOOLEAN_FILTER,
+    ACTION_TOGGLE_RATING_FILTER,
+    ACTION_CLEAR_BOOLEAN_FILTER,
+    ACTION_CLEAR_RATING_FILTER,
+    ACTION_SET_SEARCH_STRING
+    } from 'actionTypesAnswerViewer'
 
-console.log("!!!!!!!!!!!!!!!")
-//
+// cannot currently override applicationData defaults found in applicationData.js
+
 //global.window.globs['choices'] = {"rating": [{"name": "na", "description": "N/A"}, {"name": "love", "description": "Love"}, {"name": "like", "description": "Like"}, {"name": "dont_mind", "description": "Don't Mind"}, {"name": "dislike", "description": "Dislike"}, {"name": "hate", "description": "Hate"}], "booleans": [{"name": "essential", "description": "Essential"}, {"name": "curious", "description": "Curious"}, {"name": "soft_limit", "description": "Soft Limit"}, {"name": "hard_limit", "description": "Hard Limit"}, {"name": "have_done", "description": "Have Done"}]}
 //global.window.globs['choices_context'] = [{"name": "to_me", "description": "To/For Self"}, {"name": "to_others", "description": "To/For Others"}]
 //global.window.globs['csrfToken'] = "csrf_token"
 //global.window.globs['defaultLoggingLevel'] = "OFF"
+
 //var loggingLevel='ERROR'    // 'ALL'
+//window.globs = []
+//window.globs['defaultLoggingLevel'] = "WARNING"
+//
 //window.globs['loggingLevels'] =
 //{
 //    'AnswerView' : loggingLevel,
@@ -18,32 +34,66 @@ console.log("!!!!!!!!!!!!!!!")
 //    'reducers' : loggingLevel,
 //}
 //window.globs['questionsUrl'] = "questionsUrl"
-//
-test('contetRatingFilterReducer', () => {
-//
-//  let input =
-//  {
-//      "essential": true,
-//      "curious": true,
-//      "soft_limit": true,
-//      "hard_limit": true,
-//      "have_done": true
-//    }
-//  let action =
-//  {
-//      type: ACTION_SET_BOOLEAN_FILTER,
-//      id: 'have_done'
-//
-//  }
-//  let output = contextBooleanFilterReducer(input,action)
-//  expect(ouput).toEqual(
-//        {
-//          "essential": true,
-//          "curious": true,
-//          "soft_limit": true,
-//          "hard_limit": true,
-//          "have_done": false
-//        }
-//      )
+
+describe('contextBooleanFilterReducer',() => {
+    it('should return initial state', () => {
+
+        var result = contextBooleanFilterReducer(undefined, { type: '@@INIT' } )
+        console.log("result =", result)
+
+        expect(
+          contextBooleanFilterReducer(undefined, { type: '@@INIT' } )
+        ).toEqual(
+            {}
+        )
+    }
+    )
+
+    it('should set a rating', () => {
+        let input =
+        {
+            "essential": true,
+            "curious": true,
+            "soft_limit": true,
+            "hard_limit": true,
+            "have_done": true
+          }
+        let action =
+        {
+            type: ACTION_TOGGLE_BOOLEAN_FILTER,
+            id: 'have_done'
+
+        }
+        let output = contextBooleanFilterReducer(input,action)
+        expect(output).toEqual(
+              {
+                "essential": true,
+                "curious": true,
+                "soft_limit": true,
+                "hard_limit": true,
+                "have_done": false
+              }
+            )
+
+    })
+
+}
+)
+
+
+describe('contextRatingFilterReducer', () => {
+    it('should return initial state', () => {
+        var result = contextRatingFilterReducer(undefined, { type: '@@INIT' } )
+        console.log("result =", result)
+
+        expect(
+          contextRatingFilterReducer(undefined, { type: '@@INIT' } )
+        ).toEqual(
+            {}
+        )
+    }
+    )
+
+
 });
 
