33d0f9eb68193351bbefe393a4df702445ddab82
Kevin Seghetti <kts@tenetti.org>
2016-12-14 17:47:36 -0800

    added movecursor test (currently failing)

--- survey/assets/__tests__/topReducerAnswerEditor.test.js
+++ survey/assets/__tests__/topReducerAnswerEditor.test.js
@@ -1,12 +1,20 @@
 //var $ = require('jquery');
 
 import {topReducer,questionReducer} from 'topReducerAnswerEditor'
+import {navigationReducer} from 'navigationReducers'
+
 import  contextBooleanFilterReducer  from 'filterReducers'
 import * as types from 'constants/actionTypes'
 
 import {
     setField
     } from 'constants/actionTypes'
+
+
+ import {
+    moveCursorAction
+} from 'actionTypesAnswerEditor'
+
 
 
 //import {
@@ -56,6 +64,40 @@ describe('topReducer', () => {
 
 
 });
+
+describe('navigationReducer', () => {
+  it('should return the initial state', () => {
+    expect(
+      navigationReducer(undefined, { type: '@@INIT' } )
+    ).toEqual(
+        {"currentQuestion":0}
+    )
+  })
+
+  it('should move the cursor', () => {
+      let input =
+          {"currentQuestion":0}
+      let questions =
+      [{"question":{"id":1,"url":"http://localhost:8000/rest/questions/1/","question_text":"Anal Play","question_detail":"Acts in which the anus is involved."},"answers":{"to_me":{"id":1,"url":"http://localhost:8000/rest/answers/1/","user":"http://localhost:8000/rest/users/1/","question":{"id":1,"url":"http://localhost:8000/rest/questions/1/","question_text":"Anal Play","question_detail":"Acts in which the anus is involved."},"context":"to_me","essential":false,"curious":false,"soft_limit":false,"hard_limit":false,"have_done":true,"rating":"like","notes":""}}},
+       {"question":{"id":2,"url":"http://localhost:8000/rest/questions/2/","question_text":"Beating (General)","question_detail":"Acts in which one partner is beaten."}},
+       {"question":{"id":3,"url":"http://localhost:8000/rest/questions/3/","question_text":"Beating - Canes","question_detail":"Acts in which one partner is beaten with a cane."}},
+       {"question":{"id":4,"url":"http://localhost:8000/rest/questions/4/","question_text":"Beating - Crops","question_detail":"Acts in which one partner is beaten with a crop."}}
+      ]
+
+      let action = moveCursorAction(1)
+      let output = navigationReducer(input,action,questions)
+      expect(output).toEqual(
+          {"currentQuestion":1}
+      )
+
+  })
+
+
+
+});
+
+
+
 
 //global.window.globs['choices'] = {"rating": [{"name": "na", "description": "N/A"}, {"name": "love", "description": "Love"}, {"name": "like", "description": "Like"}, {"name": "dont_mind", "description": "Don't Mind"}, {"name": "dislike", "description": "Dislike"}, {"name": "hate", "description": "Hate"}], "booleans": [{"name": "essential", "description": "Essential"}, {"name": "curious", "description": "Curious"}, {"name": "soft_limit", "description": "Soft Limit"}, {"name": "hard_limit", "description": "Hard Limit"}, {"name": "have_done", "description": "Have Done"}]}
 //global.window.globs['choices_context'] = [{"name": "to_me", "description": "To/For Self"}, {"name": "to_others", "description": "To/For Others"}]
